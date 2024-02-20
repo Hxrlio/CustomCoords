8d08aa96a538b880aa6813cbf0896c082b1aaa0c
Hxrl <harley.norgate@gmail.com>
2024-02-20 19:27:07 +0000

    dinner break

--- CustomCoords/src/main/java/mc/hxrl/customcoords/event/ClientEvents.java
+++ CustomCoords/src/main/java/mc/hxrl/customcoords/event/ClientEvents.java
@@ -1,7 +1,10 @@
 package mc.hxrl.customcoords.event;
 
+import java.util.regex.Pattern;
+
 import mc.hxrl.customcoords.CustomCoords;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -16,6 +19,14 @@ public class ClientEvents {
 			
 			CustomCoords.OVERLAY.renderOverlay(e.getMatrixStack());
 			
+		}
+		
+		@SubscribeEvent
+		public static void catchChatCoords(ClientChatReceivedEvent e) {
+			String chat = e.getMessage().getString();
+			CustomCoords.LOGGER.info(chat);
+			Pattern pattern = Pattern.compile("0");
+			pattern.matcher(chat);
 		}
 	}
 }
