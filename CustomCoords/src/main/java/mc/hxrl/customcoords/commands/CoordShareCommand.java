89cd4a4550dbd416e8811f1e986bfb3d64d73345
Hxrl <harley.norgate@gmail.com>
2024-02-21 19:50:37 +0000

    added command package

---
+++ CustomCoords/src/main/java/mc/hxrl/customcoords/commands/CoordShareCommand.java
@@ -0,0 +1,16 @@
+package mc.hxrl.customcoords.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.Commands;
+
+public class CoordShareCommand {
+	
+	public CoordShareCommand(CommandDispatcher<CommandSource> dispatcher) {
+		//dispatcher.register(Commands.literal("cc").then(Commands.literal("share").executes((command) -> {
+		//	return 0;
+		//})));
+	}
+	
+}
